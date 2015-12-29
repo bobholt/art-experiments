389aff8dfbd787778c351c57f7185fdff707944b
bobholt <bobholt@gmail.com>
2015-12-29 11:16:59 -0500

    add triangles

--- src/index.js
+++ src/index.js
@@ -35,7 +35,8 @@ function describePointsOnSquare(options) {
     y: 0
   };
   var radius = options.radius || 0;
-  var distanceBetween = radius * 8 / numberOfPoints;
+  var perimeter = radius * 8;
+  var distanceBetween = perimeter / numberOfPoints;
   var counterX = radius;
   var counterY = -radius;
 
@@ -48,13 +49,54 @@ function describePointsOnSquare(options) {
     y = counterY;
 
     if (i < numberOfPoints / 4) {
-      Math.min(counterY += distanceBetween, radius);
+      counterY += distanceBetween;
     } else if (i < numberOfPoints / 2) {
-      Math.max(counterX -= distanceBetween, -radius);
+      counterX -= distanceBetween;
     } else if (i < 3 * numberOfPoints / 4) {
-      Math.max(counterY -= distanceBetween, -radius);
+      counterY -= distanceBetween;
     } else {
-      Math.min(counterX += distanceBetween, radius);
+      counterX += distanceBetween;
+    }
+
+    points.push({
+      x: x + center.x,
+      y: y + center.y,
+    });
+
+    i++;
+  }
+  return points;
+}
+
+function describePointsOnTriangle(options) {
+  var points = [];
+  var numberOfPoints = options.numberOfPoints || 72;
+  var center = options.center || {
+    x: 0,
+    y: 0
+  };
+  var radius = options.radius || 0;
+  var perimeter = (radius * 2) + radius * 2 / Math.cos(Math.PI / 3);
+  var distanceBetween = perimeter / numberOfPoints;
+  var counterX = 0;
+  var counterY = -radius;
+
+  var i = 0;
+
+  while (i < numberOfPoints) {
+    var x, y;
+
+    x = counterX;
+    y = counterY;
+
+    if (i < numberOfPoints / 3) {
+      counterX += Math.sin(Math.PI / 6) * distanceBetween;
+      counterY += Math.cos(Math.PI / 6) * distanceBetween;
+    } else if (i < 2 * numberOfPoints / 3) {
+      counterX -= distanceBetween;
+    } else {
+      counterX += Math.cos(Math.PI / 3) * distanceBetween;
+      counterY -= Math.sin(Math.PI / 3) * distanceBetween;
     }
 
     points.push({
@@ -89,6 +131,9 @@ function drawEnvelope(options) {
       break;
     case 'square':
       shapeFunction = describePointsOnSquare;
+      break;
+    case 'triangle':
+      shapeFunction = describePointsOnTriangle;
       break;
     default:
       shapeFunction = describePointsOnCircle;
@@ -205,5 +250,47 @@ function drawEnvelope(options) {
     id: 'square-six',
     numberOfPoints: 18,
     shape: 'square',
+  });
+
+  drawEnvelope({
+    calc: function(i) { return i + 12; },
+    id: 'triangle-one',
+    numberOfPoints: 36,
+    shape: 'triangle',
+  });
+
+  drawEnvelope({
+    calc: cardioid,
+    id: 'triangle-two',
+    numberOfPoints: 72,
+    shape: 'triangle',
+  });
+
+  drawEnvelope({
+    calc: nephroid,
+    id: 'triangle-three',
+    numberOfPoints: 108,
+    shape: 'triangle',
+  });
+
+  drawEnvelope({
+    calc: function(i) { return Math.pow(i, 2); },
+    id: 'triangle-four',
+    numberOfPoints: 90,
+    shape: 'triangle',
+  });
+
+  drawEnvelope({
+    calc: function(i) { return Math.pow(i, 3); },
+    id: 'triangle-five',
+    numberOfPoints: 180,
+    shape: 'triangle',
+  });
+
+  drawEnvelope({
+    calc: function(i) { return Math.pow(i, 4); },
+    id: 'triangle-six',
+    numberOfPoints: 18,
+    shape: 'triangle',
   });
 }());
